7c34ca7dc51ec8f2bd5a53c5661dd1d653f0dc9a
Rareș Cosma <rares@getbetter.ro>
2025-12-04 00:13:45 +0100

    03

---
+++ 03.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("inputs/03.in")
+	scanner := bufio.NewScanner(file)
+
+	p1 := 0
+	p2 := 0
+	for scanner.Scan() {
+		bat := scanner.Bytes()
+		p1 += gobble(bat, 2)
+		p2 += gobble(bat, 12)
+	}
+
+	println("p1:", p1)
+	println("p2:", p2)
+}
+
+func digitIndexes(b []byte) map[int][]int {
+	ret := make(map[int][]int, 10)
+	for i, c := range b {
+		digit := int(c - '0')
+		ret[digit] = append(ret[digit], i)
+	}
+	return ret
+}
+
+func gobble(b []byte, numNeeded int) int {
+	indexes := digitIndexes(b)
+
+	joltage := 0
+	lastIdx := -1
+	for numNeeded > 0 {
+	out:
+		for digit := 9; digit > 0; digit-- {
+			for _, idx := range indexes[digit] {
+				if idx <= len(b)-numNeeded && idx > lastIdx {
+					joltage = joltage*10 + digit
+					numNeeded--
+					lastIdx = idx
+					break out
+				}
+			}
+		}
+	}
+
+	return joltage
+}
