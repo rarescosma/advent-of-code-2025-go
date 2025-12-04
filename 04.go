e2a6ce77eaa5969e58ef1e9f2e4beb93d26f0c22
Rareș Cosma <rares@getbetter.ro>
2025-12-04 23:19:05 +0100

    04: structs

--- 04.go
+++ 04.go
@@ -5,37 +5,61 @@ import (
 	"os"
 )
 
+type Pos struct {
+	row int
+	col int
+}
+
+type Map struct {
+	buf     [][]byte
+	numRows int
+	numCols int
+}
+
+func (m *Map) Get(p Pos) byte {
+	return m.buf[p.row][p.col]
+}
+
+func (m *Map) Set(p Pos, b byte) {
+	m.buf[p.row][p.col] = b
+}
+
+func MapFromScanner(scanner *bufio.Scanner) Map {
+	var buf [][]byte
+	numRows := 0
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		buf = append(buf, make([]byte, 0))
+		buf[numRows] = append(buf[numRows], line...)
+		numRows++
+	}
+	numCols := len(buf[0])
+	return Map{buf, numRows, numCols}
+}
+
 func main() {
 	file, _ := os.Open("inputs/04.in")
 	scanner := bufio.NewScanner(file)
 
-	var theMap [][]byte
-	numRows := 0
-	for scanner.Scan() {
-		line := scanner.Bytes()
-		theMap = append(theMap, make([]byte, 0))
-		theMap[numRows] = append(theMap[numRows], line...)
-		numRows++
-	}
-	numCols := len(theMap[0])
+	theMap := MapFromScanner(scanner)
 
-	p1 := removeRolls(&theMap, numRows, numCols)
+	p1 := removeRolls(&theMap)
 	p2, removed := p1, p1
 
 	for removed > 0 {
-		removed = removeRolls(&theMap, numRows, numCols)
+		removed = removeRolls(&theMap)
 		p2 += removed
 	}
 	println("p1:", p1)
 	println("p2:", p2)
 }
 
-func removeRolls(m *[][]byte, numRows int, numCols int) int {
+func removeRolls(m *Map) int {
 	ret := 0
-	var toRemove [][2]int
-	for x := 0; x < numCols; x++ {
-		for y := 0; y < numRows; y++ {
-			if (*m)[x][y] != '@' {
+	var toRemove []Pos
+	for x := 0; x < m.numCols; x++ {
+		for y := 0; y < m.numRows; y++ {
+			if m.Get(Pos{x, y}) != '@' {
 				continue
 			}
 			neighs := 0
@@ -45,19 +69,19 @@ func removeRolls(m *[][]byte, numRows int, numCols int) int {
 						continue
 					}
 					nx, ny := x+dx, y+dy
-					if numRows > nx && nx >= 0 && numCols > ny && ny >= 0 && (*m)[nx][ny] == '@' {
+					if m.numRows > nx && nx >= 0 && m.numCols > ny && ny >= 0 && m.Get(Pos{nx, ny}) == '@' {
 						neighs += 1
 					}
 				}
 			}
 			if neighs < 4 {
-				toRemove = append(toRemove, [2]int{x, y})
+				toRemove = append(toRemove, Pos{x, y})
 				ret += 1
 			}
 		}
 	}
-	for _, xy := range toRemove {
-		(*m)[xy[0]][xy[1]] = 'x'
+	for _, pos := range toRemove {
+		m.Set(pos, 'x')
 	}
 	return ret
 }
