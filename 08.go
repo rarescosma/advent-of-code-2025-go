c8d4ad8d8a46606926e72c1ba1d9291fc0c504f4
Rareș Cosma <rares@getbetter.ro>
2025-12-08 12:15:06 +0100

    08

---
+++ 08.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"maps"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type Junc struct {
+	x, y, z int
+}
+
+type JuncPair struct {
+	left, right JuncIdx
+}
+
+type JuncIdx = int
+type JuncDist = int
+type DistMap = map[JuncDist][]JuncPair
+
+type CircuitIdx = int
+type CircuitMap = map[JuncIdx]CircuitIdx
+
+func main() {
+	takeDists := 1000
+	file, _ := os.Open("inputs/08.in")
+	scanner := bufio.NewScanner(file)
+
+	var juncs []Junc
+	juncIdx := 0
+	distMap := make(DistMap)
+	for scanner.Scan() {
+		junc := getJunc(scanner.Text())
+		juncs = append(juncs, junc)
+		for other := range juncIdx {
+			key := dist(junc, juncs[other])
+			distMap[key] = append(distMap[key], JuncPair{juncIdx, other})
+		}
+		juncIdx++
+	}
+
+	circuits := make(CircuitMap)
+	for juncIdx := range juncs {
+		circuits[juncIdx] = juncIdx
+	}
+
+	p1, p2, numDists, totalJuncs := 1, 0, 0, len(juncs)
+out:
+	for _, dist := range slices.Sorted(maps.Keys(distMap)) {
+		for _, pair := range distMap[dist] {
+			c1, c2 := circuits[pair.left], circuits[pair.right]
+			if c1 == c2 {
+				continue
+			}
+
+			numJuncs := 0
+			for k, v := range circuits {
+				if v == c1 {
+					numJuncs++
+				}
+				if v == c2 {
+					circuits[k] = c1
+					numJuncs++
+				}
+			}
+			if numJuncs == totalJuncs {
+				p2 = juncs[pair.left].x * juncs[pair.right].x
+				if numDists > takeDists {
+					break out
+				}
+			}
+		}
+		numDists++
+
+		if numDists == takeDists {
+			tally := make(map[CircuitIdx]int)
+			for _, circuitIdx := range circuits {
+				tally[circuitIdx]++
+			}
+			for _, circuitSize := range slices.Sorted(maps.Values(tally))[len(tally)-3:] {
+				p1 *= circuitSize
+			}
+		}
+	}
+
+	println("p1:", p1)
+	println("p2:", p2)
+}
+
+func getJunc(s string) Junc {
+	var coords []int
+	for _, el := range strings.Split(s, ",") {
+		if el != "" {
+			coord, _ := strconv.Atoi(el)
+			coords = append(coords, coord)
+		}
+	}
+	return Junc{coords[0], coords[1], coords[2]}
+}
+
+func dist(j1, j2 Junc) JuncDist {
+	return (j1.x-j2.x)*(j1.x-j2.x) + (j1.y-j2.y)*(j1.y-j2.y) + (j1.z-j2.z)*(j1.z-j2.z)
+}
