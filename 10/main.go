66098e0a4230b59cb3f2a1233e8b6376e6023bf0
Rareș Cosma <rares@getbetter.ro>
2025-12-13 23:13:59 +0200

    10: optimize

--- 10/main.go
+++ 10/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"bufio"
-	"encoding/binary"
-	"hash/fnv"
 	"math"
 	"os"
 	"strconv"
@@ -11,8 +9,7 @@ import (
 )
 
 type State = []int
-
-type Move = []int
+type Button = []int
 
 func main() {
 	file, _ := os.Open("inputs/10.in")
@@ -20,207 +17,149 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	p1, p2 := 0, 0
+
 	for scanner.Scan() {
-		line := scanner.Text()[1:]
-		line = line[:len(line)-1]
-		parts := strings.Split(line, "] (")
-		goal := make([]int, len(parts[0]))
+		line := scanner.Text()
+		parts := strings.Split(line[1:len(line)-1], "] (")
+
+		p1Goal := make(State, len(parts[0]))
 		for i, c := range parts[0] {
 			if c == '#' {
-				goal[i] = 1
+				p1Goal[i] = 1
 			}
 		}
 
 		parts = strings.Split(parts[1], ") {")
+		p2Goal := intsPlease(parts[1])
 
-		var p2Goal []int
-		for _, val := range intsPlease(parts[1]) {
-			p2Goal = append(p2Goal, val)
+		buttonParts := strings.Split(parts[0], ") (")
+		buttons := make([]Button, len(buttonParts))
+		for i, s := range buttonParts {
+			buttons[i] = intsPlease(s)
 		}
 
-		var moves []Move
-		for _, part := range strings.Split(parts[0], ") (") {
-			moves = append(moves, intsPlease(part))
-		}
-		pCache := make(map[uint32][][]Move)
-		minMoves := math.MaxInt
-		for _, combo := range patternsFor(goal, moves, &pCache) {
-			if len(combo) < minMoves {
-				minMoves = len(combo)
-			}
-		}
-		p1 += minMoves
-		cache := make(map[uint32]int)
-		cache[stateHash(make(State, len(goal)))] = 0
-		p2 += p2Solve(p2Goal, moves, &cache, &pCache)
+		parityMap := make(map[uint64][][]int)
+		p1 += p1Solve(p1Goal, buttons, &parityMap)
+
+		cache := make(map[uint64]int)
+		cache[hashState(make(State, len(p2Goal)))] = 0
+		p2 += p2Solve(p2Goal, buttons, parityMap, cache)
 	}
 
 	println("p1:", p1)
 	println("p2:", p2)
 }
 
-func p2Solve(goal []int, moves []Move, cache *map[uint32]int, pCache *map[uint32][][]Move) int {
-	key := stateHash(goal)
-	if res, ok := (*cache)[key]; ok {
+func p1Solve(goal State, buttons []Button, parityMap *map[uint64][]Button) int {
+	nButtons := len(buttons)
+	limit := 1 << nButtons
+	ret := math.MaxInt
+
+	currentParity := make([]int, len(goal))
+
+	for i := 0; i < limit; i++ {
+		for k := range currentParity {
+			currentParity[k] = 0
+		}
+
+		var bits []int
+
+		for bit := 0; bit < nButtons; bit++ {
+			if (i & (1 << bit)) != 0 {
+				bits = append(bits, bit)
+				for _, pos := range buttons[bit] {
+					currentParity[pos] ^= 1
+				}
+			}
+		}
+
+		numBits := len(bits)
+		if numBits < ret && stateEq(currentParity, goal) {
+			ret = numBits
+		}
+
+		h := hashState(currentParity)
+		(*parityMap)[h] = append((*parityMap)[h], bits)
+	}
+	return ret
+}
+
+func p2Solve(goal State, buttons []Button, parityMap map[uint64][]Button, cache map[uint64]int) int {
+	key := hashState(goal)
+	if res, ok := cache[key]; ok {
 		return res
 	}
 
-	state := p2Goal2State(&goal)
-	combos := patternsFor(state, moves, pCache)
+	ret := math.MaxInt
 
-	var rec []int
+	nextGoal := make([]int, len(goal))
 out:
-	for _, combo := range combos {
-		newGoal := make(State, len(goal))
-		copy(newGoal, goal)
-		for _, move := range combo {
-			p2Transform(&newGoal, move)
+	for _, bits := range parityMap[hashParity(goal)] {
+		copy(nextGoal, goal)
+
+		for _, bit := range bits {
+			for _, pos := range buttons[bit] {
+				nextGoal[pos]--
+			}
 		}
-		for _, digit := range newGoal {
-			if digit < 0 {
+
+		for i, v := range nextGoal {
+			if v < 0 {
 				continue out
 			}
+			nextGoal[i] = v / 2
 		}
-		for i := range newGoal {
-			newGoal[i] /= 2
-		}
-		solve := p2Solve(newGoal, moves, cache, pCache)
-		if solve != math.MaxInt {
-			rec = append(rec, 2*solve+len(combo))
-		}
-	}
 
-	ret := math.MaxInt
-	for i := 0; i < len(rec); i++ {
-		ret = min(ret, rec[i])
-	}
-	(*cache)[key] = ret
-	return ret
-}
-
-func p2Goal2State(goal *State) State {
-	ret := make([]int, len(*goal))
-	for i, digit := range *goal {
-		ret[i] = digit % 2
-	}
-	return ret
-}
-
-func patternsFor(goal State, moves []Move, pCache *map[uint32][][]Move) [][]Move {
-	key := stateHash(goal) ^ movesHash(moves)
-	if ret, ok := (*pCache)[key]; ok {
-		return ret
-	}
-
-	var ret [][]Move
-	for r := 0; r <= len(moves); r++ {
-		for _, combo := range combinations(moves, r) {
-			state := make(State, len(goal))
-			for _, move := range combo {
-				p1Transform(&state, move)
-			}
-			if stateEq(state, goal) {
-				ret = append(ret, combo)
+		if solve := p2Solve(nextGoal, buttons, parityMap, cache); solve != math.MaxInt {
+			total := 2*solve + len(bits)
+			if total < ret {
+				ret = total
 			}
 		}
 	}
-	(*pCache)[key] = ret
+
+	cache[key] = ret
 	return ret
 }
 
-func p2Transform(state *State, move Move) {
-	for _, pos := range move {
-		(*state)[pos]--
-	}
+func hashState(s State) uint64 {
+	return fastHash(s, func(v int) uint64 {
+		return uint64(v)
+	})
 }
 
-func p1Transform(state *State, move Move) {
-	for _, pos := range move {
-		(*state)[pos] ^= 1
-	}
+func hashParity(s State) uint64 {
+	return fastHash(s, func(v int) uint64 {
+		return uint64(v % 2)
+	})
 }
 
-func intsPlease(s string) []int {
-	return nonEmpties(s, func(s string) int { res, _ := strconv.Atoi(s); return res })
-}
-
-func nonEmpties[T any](s string, f func(string) T) []T {
-	var ret []T
-	for _, el := range strings.Split(s, ",") {
-		if el != "" {
-			ret = append(ret, f(el))
-		}
+func fastHash(s []int, fn func(v int) uint64) uint64 {
+	var h uint64 = 14695981039346656037
+	for _, v := range s {
+		h ^= fn(v)
+		h *= 1099511628211
 	}
-	return ret
+	return h
 }
 
 func stateEq(s1, s2 State) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
-	for i, d := range s1 {
-		if d != s2[i] {
+	for i := range s1 {
+		if s1[i] != s2[i] {
 			return false
 		}
 	}
 	return true
 }
 
-func movesHash(moves []Move) uint32 {
-	k := uint32(0)
-	for _, m := range moves {
-		k ^= stateHash(m)
+func intsPlease(s string) []int {
+	var ret []int
+	for _, el := range strings.Split(s, ",") {
+		val, _ := strconv.Atoi(el)
+		ret = append(ret, val)
 	}
-	return k
-}
-
-func stateHash(s State) uint32 {
-	hasher := fnv.New32()
-	for _, el := range s {
-		buf := make([]byte, 5)
-		buf[4] = '|'
-		binary.BigEndian.PutUint32(buf, uint32(el))
-		_, _ = hasher.Write(buf)
-	}
-	return hasher.Sum32()
-}
-
-func combinations[C any](it []C, r int) [][]C {
-	n := len(it)
-	if r > n {
-		return [][]C{}
-	}
-
-	var indices []int
-	for x := range r {
-		indices = append(indices, x)
-	}
-
-	take := func(xs []int) []C {
-		row := make([]C, r)
-		for i, x := range xs {
-			row[i] = it[x]
-		}
-		return row
-	}
-
-	var ret [][]C
-	ret = append(ret, take(indices))
-	for {
-		found := -1
-		for i := r - 1; i >= 0; i-- {
-			if indices[i] != i+n-r {
-				found = i
-				break
-			}
-		}
-		if found < 0 {
-			return ret
-		}
-		indices[found]++
-		for j := found + 1; j < r; j++ {
-			indices[j] = indices[j-1] + 1
-		}
-		ret = append(ret, take(indices))
-	}
+	return ret
 }
